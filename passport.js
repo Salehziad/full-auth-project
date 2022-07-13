72e1e77145b56ee21e0965f02fce58d6e39f9f17
Salehziad <salehziad1999@gmail.com>
2022-07-13 15:47:12 +0300

    server

--- passport.js
+++ passport.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const GithubStrategy = require("passport-github2").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
 
-const GOOGLE_CLIENT_ID =
-  "598131306396-9gnjdp5b69tfina12hi1rrr88s6h2ttn.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
+const GOOGLE_CLIENT_ID =process.env.GOOGLE_CLIENT_ID;
 
-GITHUB_CLIENT_ID = "6ebfe5a24ad1276f0545";
-GITHUB_CLIENT_SECRET = "7611d166a2856ae50141ba22ee79aaf0f8f2a0bc";
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 FACEBOOK_APP_ID = "your id";
 FACEBOOK_APP_SECRET = "your id";
