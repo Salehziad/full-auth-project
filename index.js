05b66a2f652df5bbdfdf440c8eba2c9f58011a67
Salehziad <salehziad1999@gmail.com>
2022-07-16 16:01:53 +0300

    admin

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(
     cors({
-      // origin:"http://localhost:3000/"
+      origin:"http://localhost:3000/"
       // origin: "https://salehziad-projects.netlify.app",
       methods: "GET,POST,PUT,DELETE",
       credentials: true,
