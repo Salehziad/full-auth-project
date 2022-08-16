4223fab1015eec5fed84bb353cc74b99324f9d3c
Salehziad <salehziad1999@gmail.com>
2022-08-16 11:15:30 +0300

    deploy

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ const cookieSession = require('cookie-session');
 const express = require('express');
 const cors = require('cors');
 const passport = require('passport');
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 const pasportSetUp = require('./passport');
 const router = require('./routes/auth');
 const authRouter=require('./auth/router/index');
-// const logsRoute=require('./auth/logs/logsRoute');
 const {db}=require('./models-connections');
 const session= require('express-session');
 const app = express();
 app.use(express.json());
 app.set("trust proxy", 1);
-// app.use(
-//   cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
-// );
+
 app.use(
   session({
     secret: "secretcode",
@@ -32,20 +29,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(
     cors({
-      origin:"https://auth-salehziad.netlify.app",
+      origin: "https://remarkable-babka-9104f5.netlify.app",
       methods: "GET,POST,PUT,DELETE",
       credentials: true,
     })
   );
 app.use('/auth', router);
 app.use('/auth',authRouter);
-// app.use('/admin',logsRoute);
-app.get('/',(req, res) => {
-  res.send('welcome')
-})
 db.sync()
 .then(() => {
-    app.listen(port, () => {
-        console.log(`server is lestining 0n port ${port}`);
+    app.listen(PORT, () => {
+        console.log(`server is lestining 0n port ${PORT}`);
     });
   });
